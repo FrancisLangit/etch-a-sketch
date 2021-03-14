96e474237b3a93927d7af36de05b1130b0e97649
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-14 16:00:07 +0800

    Add functionality to change size button

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 function setUpGridContainer(size) {
-    /**Sets the number of columns in the grid container equal to "size" 
-     * argument, of which is either 16 (by default) or as inputted by user.*/
+    /**Sets number of columns in grid container equal to size.*/
     const gridContainer = document.getElementById('grid-container');
     gridContainer.style.setProperty(
         'grid-template-columns', `repeat(${size}, 1fr)`);
     return gridContainer;
 }
 
-function createGrid(size) {
-    /**Fills up the grid container with square divs respective to "size" 
-     * argument. Such is equal to both the length and width of the grid 
-     * container in number of square divs.*/
+function fillUpGrid(size) {
+    /**Fills up the grid container with size^2 grid-item divs.*/
     const gridContainer = setUpGridContainer(size);    
     for (i = 0; i < size**2; i++) {
         const gridItem = document.createElement('div');
@@ -19,4 +16,16 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
+
+function changeGridSize() {
+    const changeSizeButton = document.getElementById('change-size-button');
+    changeSizeButton.addEventListener("click", () => {
+        const newSize = prompt("Enter new size.");
+        const gridContainer = document.getElementById('grid-container');
+        gridContainer.innerHTML = '';
+        fillUpGrid(newSize);
+    });
+}
+
+changeGridSize();
+fillUpGrid(16);
