517db3ad6b0378b67c0bcacf79c021b8b790160c
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-14 18:12:41 +0800

    Construct funtionality for rainbow mode
    - getRandomHex() created to generate random hex values.
    - Rainbow mode is activated if and when this.isRainbow is true.

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 class EtchASketch {
     constructor() {
+        this.isRainbow = true;
+        this.isGreyscale = false; 
         this.defaultSize = 16;
         this.gridContainer = document.getElementById('grid-container');
     }
     
+    getRandomHex() {
+        /**Generates a random hexidecimal value in the form of a string.
+         * Original by aravk33 (source: https://stackoverflow.com/questions/50
+         * 92808/how-do-i-randomly-generate-html-hex-color-codes-using-javascr
+         * ipt).*/
+        return '#' + (
+            Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
+    }
+
     changeGridItemColor(gridItem) {
-        /**Changes background of gridItem to a different color.*/
-        gridItem.style.setProperty('background', 'black');
+        /**Changes background-color of gridItem to a different color.*/
+        if (this.isGreyscale) {
+            gridItem.style.setProperty('background-color', 'black');
+        } else if (this.isRainbow) {
+            gridItem.style.setProperty('background-color', this.getRandomHex());
+        } else {
+            gridItem.style.setProperty('background-color', 'black');
+        } 
     }
 
     createGridItem() {
@@ -20,8 +37,8 @@ class EtchASketch {
     }
 
     createGrid(size) {
-        /** Sets number of columns in grid container equal to size argument
-         *  and fills such up with size**2 grid-item divs.*/
+        /**Sets number of columns in grid container equal to size argument
+         * and fills such up with size**2 grid-item divs.*/
         this.gridContainer.style.setProperty(
             'grid-template-columns', `repeat(${size}, 1fr)`);
         for (let i = 0; i < size**2; i++) {
@@ -43,13 +60,13 @@ class EtchASketch {
     }
 
     run() {
-        /**Runs the program. Adds event listener to change size button and 
+        /**Runs the program. Adds event listener to change size button and
          * creates a 16x16 grid by default.*/
         const changeSizeButton = document.getElementById(
             'change-size-button');
         changeSizeButton.addEventListener(
             'click', () => this.promptNewGridSize());
-        this.createGrid(16);
+        this.createGrid(this.defaultSize);
     }
 }
 
