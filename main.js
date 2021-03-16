bc1068a96343ebec4516be5fe291ef4da9374342
Francis Langit <francis.villanueva.langit@gmail.com>
2021-03-16 13:30:12 +0800

    Create toggleGrid() method
    Such makes the canvas active or inactive.

--- main.js
+++ main.js
@@ -43,11 +43,11 @@ class EtchASketch {
         });
     }
 
-    configureToggleGridButton() {
-        /**Adds event listener to toggle grid button that makes it toggle the
-         * borders on all the grid squares on and off.*/
+    configureToggleBordersButton() {
+        /**Adds event listener to toggle borders button that makes it toggle 
+         * the borders on all the grid squares on and off.*/
         const toggleGridButton = document.getElementById(
-            'toggle-grid-button');
+            'toggle-borders-button');
         toggleGridButton.addEventListener('click', () => {
             const gridItems = document.querySelectorAll('.grid-item');
             for (let i = 0; i < gridItems.length; i++) {
@@ -56,22 +56,34 @@ class EtchASketch {
         }); 
     }
 
+    toggleGrid(isGridActive) {
+        /**Adds mouseover event listener calling colorGridItem() to all grid
+         * items if isGridActive true. Otherwise, replaces all grid items with
+         * a copy of themselves without any event listeners.*/
+        this.isGridActive = isGridActive
+        const gridItems = document.querySelectorAll('.grid-item');
+        for (let i = 0; i < gridItems.length; i++) {
+            if (isGridActive) {
+                gridItems[i].addEventListener(
+                    'mouseover', () => this.colorGridItem(gridItems[i]));
+            } else {
+                gridItems[i].replaceWith(gridItems[i].cloneNode(true));
+            }
+        }
+    }
+
     configureGridToggling() {
-        /**Adds event listener to window. If grid-container div is clicked, 
-         * sets this.isGridActive to true. Otherwise, if a click is made
-         * outside the the grid-container div, this.isGridActive set to 
-         * false.*/
+        /**Adds event listener to window that checks if a mouse click is made
+         * inside or outside #grid-conatiner div. Passes true to toggleGrid()
+         * if former. If latter, passes false.*/
         window.addEventListener('click', (e) => {
             if (this.gridContainer.contains(e.target)) {
-                this.isGridActive = true;
-                console.log(this.isGridActive);
+                this.toggleGrid(true);
             } else {
-                this.isGridActive = false;
-                console.log(this.isGridActive);
+                this.toggleGrid(false);
             }
         });
     }
-
 
     darkenGridItemColor(gridItem) {
         /**Subtracts 10% brightness from the gridItem div using its "filter" 
@@ -89,9 +101,9 @@ class EtchASketch {
             Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
     }
 
-    changeGridItemColor(gridItem) {
-        /**Changes background-color of gridItem to a different color dependent
-         * on boolean values of this.isGreyscale and this.isRainbow.*/
+    colorGridItem(gridItem) {
+        /**Changes background of gridItem to a different color dependent on 
+         * boolean values of this.isGreyscale and this.isRainbow.*/
         if (this.isGreyscale) {
             this.darkenGridItemColor(gridItem);
         } else if (this.isRainbow) {
@@ -105,11 +117,9 @@ class EtchASketch {
 
     createGridItem() {
         /**Creates a div with class grid-item and a mouseover event listener
-         * that calls changeGridItemColor().*/
+         * that calls colorGridItem().*/
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item', 'grid-item-border');
-        gridItem.addEventListener(
-            'mouseover', () => this.changeGridItemColor(gridItem));
         this.gridContainer.appendChild(gridItem);
     }
 
@@ -128,7 +138,7 @@ class EtchASketch {
          * a 16x16 grid by default.*/
         this.configureChangeSizeButton();
         this.configureClearCanvasButton();
-        this.configureToggleGridButton();
+        this.configureToggleBordersButton();
         this.configureGridToggling();
         this.createGrid(this.defaultSize);
     }
